8aa5af80e23dcc738bbbaba177f76af979128296
Alan <iimoodesign.web@gmail.com>
2023-11-07 22:46:21 +0800

    update about and portfolio display

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,6 +3,28 @@ const jsonDataArray = JSON.parse(localStorage.getItem('savedVariable'));
 
 jsonDataArray.reverse();
 
+function splitTextWithPunctuation(text) {
+  const punctuations = [',', '，', '。', '！', '？', '：', '；', '♡', '.'];
+  let result = '';
+  let currentLine = '';
+
+  for (let i = 0; i < text.length; i++) {
+      currentLine += text[i];
+      if (punctuations.includes(text[i])) {
+          result += currentLine + '\n';
+          currentLine = '';
+      }
+  }
+
+  if (currentLine !== '') {
+      result += currentLine + '\n';
+  }
+
+  return result;
+}
+
+
+
 function showAll(){
     jsonDataArray.forEach((jsonData, index) => {
         const col = document.createElement('div');
@@ -26,7 +48,7 @@ function showAll(){
 
         const image = document.createElement('img');
         image.className = 'img-fluid';
-        image.setAttribute('src', jsonData.圖片連結);
+        image.setAttribute('src', jsonData.圖片連結 + `1.jpg`);
         image.setAttribute('alt', '');
 
         link.appendChild(hover);
@@ -36,7 +58,7 @@ function showAll(){
         caption.className = 'portfolio-caption';
 
         const title = document.createElement('h4');
-        title.innerText = jsonData.作品名稱;
+        title.innerText = jsonData.專案名;
 
         const subtitle = document.createElement('p');
         subtitle.className = 'text-muted';
@@ -53,8 +75,19 @@ function showAll(){
         portfolioContainer.appendChild(col);
     });
 
-
     jsonDataArray.forEach((jsonData, index) => {
+        const image_item = [];
+        for(let i = 0; i <=6 ; i++){
+          const image = document.createElement('img');
+          image.className = 'img-fluid d-block mx-auto';
+          image.setAttribute('src', jsonData.圖片連結 + `${i}.jpg`);
+          image.onerror = function(){
+            image.remove();
+          }
+          image_item.push(image);
+          console.log(image);
+        }
+
         const modal = document.createElement('div');
         modal.className = 'portfolio-modal modal fade';
         modal.id = (`p${index + 1}`);
@@ -96,37 +129,36 @@ function showAll(){
         const modalBody = document.createElement('div');
         modalBody.className = 'modal-body';
 
-        const title = document.createElement('h2');
-        title.className = 'text-uppercase';
-        title.innerText = jsonData.作品名稱;
+        // const title = document.createElement('h2');
+        // title.className = 'text-uppercase';
+        // title.innerText = jsonData.副標題;
 
-        const subtitle = document.createElement('p');
-        subtitle.className = 'item-intro text-muted';
-        subtitle.innerText = jsonData.副標題;
-
-        const image = document.createElement('img');
-        image.className = 'img-fluid d-block mx-auto';
-        image.setAttribute('src', jsonData.圖片連結);
-        image.setAttribute('alt', 'Shirts on a hanger');
-
+        // const subtitle = document.createElement('p');
+        // subtitle.className = 'item-intro text-muted';
+        // subtitle.innerText = jsonData.副標題;
+        
         const description = document.createElement('p');
-        description.innerText = jsonData.描述;
+        description.innerText = splitTextWithPunctuation(jsonData.描述);
 
         const ul = document.createElement('ul');
         ul.className = 'list-inline';
 
+        const locateLi = document.createElement('li');
+        locateLi.innerText = `區域： ${jsonData.區域}`;
+
         const dateLi = document.createElement('li');
-        dateLi.innerText = `日期: ${jsonData.日期}`;
+        dateLi.innerText = `年份： ${jsonData.日期}`;
 
-        const clientLi = document.createElement('li');
-        clientLi.innerText = `客戶: ${jsonData.客戶}`;
+        const sizeLi = document.createElement('li');
+        sizeLi.innerText = `坪數： ${jsonData.坪數}坪`;
 
-        const categoryLi = document.createElement('li');
-        categoryLi.innerText = `種類: ${jsonData.種類}`;
+        const projectNameLi = document.createElement('li');
+        projectNameLi.innerText = `專案名： ${jsonData.專案名}`;
 
+        ul.appendChild(locateLi);
         ul.appendChild(dateLi);
-        ul.appendChild(clientLi);
-        ul.appendChild(categoryLi);
+        ul.appendChild(sizeLi);
+        ul.appendChild(projectNameLi);
 
         const leaveButton = document.createElement('button');
         leaveButton.className = 'btn';
@@ -141,11 +173,16 @@ function showAll(){
         leaveButton.appendChild(leaveIcon);
         leaveButton.appendChild(document.createTextNode('離開'));
 
-        modalBody.appendChild(title);
-        modalBody.appendChild(subtitle);
-        modalBody.appendChild(image);
-        modalBody.appendChild(description);
+        // modalBody.appendChild(title);
+        // modalBody.appendChild(subtitle);
+        modalBody.appendChild(image_item[0]);
         modalBody.appendChild(ul);
+        modalBody.appendChild(description);
+        for(let j = 1; j <= 6; j++){
+          if(image_item[j] != null)
+            modalBody.appendChild(image_item[j]);
+        }
+        
         modalBody.appendChild(document.createElement('p'));
         modalBody.appendChild(leaveButton);
 
@@ -165,160 +202,176 @@ function showAll(){
 
 function showTopN(n){
     jsonDataArray.slice(0,n).forEach((jsonData, index) => {
-        const col = document.createElement('div');
-        col.className = 'col-md-4 col-sm-6 portfolio-item';
+      const col = document.createElement('div');
+      col.className = 'col-md-4 col-sm-6 portfolio-item';
 
-        const link = document.createElement('a');
-        link.className = 'portfolio-link';
-        link.setAttribute('data-toggle', 'modal');
-        link.setAttribute('href', `#p${index + 1}`);
+      const link = document.createElement('a');
+      link.className = 'portfolio-link';
+      link.setAttribute('data-toggle', 'modal');
+      link.setAttribute('href', `#p${index + 1}`);
 
-        const hover = document.createElement('div');
-        hover.className = 'portfolio-hover';
+      const hover = document.createElement('div');
+      hover.className = 'portfolio-hover';
 
-        const hoverContent = document.createElement('div');
-        hoverContent.className = 'portfolio-hover-content';
+      const hoverContent = document.createElement('div');
+      hoverContent.className = 'portfolio-hover-content';
 
-        const plusIcon = document.createElement('i');
-        plusIcon.className = 'fas fa-plus fa-3x';
-        hoverContent.appendChild(plusIcon);
-        hover.appendChild(hoverContent);
+      const plusIcon = document.createElement('i');
+      plusIcon.className = 'fas fa-plus fa-3x';
+      hoverContent.appendChild(plusIcon);
+      hover.appendChild(hoverContent);
 
-        const image = document.createElement('img');
-        image.className = 'img-fluid';
-        image.setAttribute('src', jsonData.圖片連結);
-        image.setAttribute('alt', '');
+      const image = document.createElement('img');
+      image.className = 'img-fluid';
+      image.setAttribute('src', jsonData.圖片連結 + `1.jpg`);
+      image.setAttribute('alt', '');
 
-        link.appendChild(hover);
-        link.appendChild(image);
+      link.appendChild(hover);
+      link.appendChild(image);
 
-        const caption = document.createElement('div');
-        caption.className = 'portfolio-caption';
+      const caption = document.createElement('div');
+      caption.className = 'portfolio-caption';
 
-        const title = document.createElement('h4');
-        title.innerText = jsonData.作品名稱;
+      const title = document.createElement('h4');
+      title.innerText = jsonData.專案名;
 
-        const subtitle = document.createElement('p');
-        subtitle.className = 'text-muted';
-        subtitle.innerText = jsonData.副標題;
+      const subtitle = document.createElement('p');
+      subtitle.className = 'text-muted';
+      subtitle.innerText = jsonData.副標題;
 
-        caption.appendChild(title);
-        caption.appendChild(subtitle);
+      caption.appendChild(title);
+      caption.appendChild(subtitle);
 
-        col.appendChild(link);
-        col.appendChild(caption);
+      col.appendChild(link);
+      col.appendChild(caption);
 
-        // 將新建的元素添加到頁面中的適當位置
-        const portfolioContainer = document.querySelector('.portfolio-container'); // 假設有一個包含這些區塊的容器元素
-        portfolioContainer.appendChild(col);
+      // 將新建的元素添加到頁面中的適當位置
+      const portfolioContainer = document.querySelector('.portfolio-container'); // 假設有一個包含這些區塊的容器元素
+      portfolioContainer.appendChild(col);
     });
 
-    jsonDataArray.forEach((jsonData, index) => {
-        const modal = document.createElement('div');
-        modal.className = 'portfolio-modal modal fade';
-        modal.id = (`p${index + 1}`);
-        modal.tabIndex = '-1';
-        modal.role = 'dialog';
-        modal.style.display = 'none';
-        modal.setAttribute('aria-hidden', 'true');
-
-        const modalDialog = document.createElement('div');
-        modalDialog.className = 'modal-dialog';
-
-        const modalContent = document.createElement('div');
-        modalContent.className = 'modal-content';
-
-        const closeButtonContainer = document.createElement('div');
-        closeButtonContainer.style.position = 'relative';
-
-        const closeButton = document.createElement('button');
-        closeButton.className = 'btn leave-button';
-        closeButton.setAttribute('data-dismiss', 'modal');
-        closeButton.setAttribute('type', 'button');
-        closeButton.setAttribute('onmouseover', "this.style.backgroundColor='lightgray';");
-        closeButton.setAttribute('onmouseout', "this.style.backgroundColor='rgb(255, 255, 255, 0)';");
-
-        const closeIcon = document.createElement('i');
-        closeIcon.className = 'fas fa-times';
-        closeButton.appendChild(closeIcon);
-        closeButtonContainer.appendChild(closeButton);
-
-        const container = document.createElement('div');
-        container.className = 'container';
-
-        const row = document.createElement('div');
-        row.className = 'row';
-
-        const col = document.createElement('div');
-        col.className = 'col-lg-8 mx-auto';
-
-        const modalBody = document.createElement('div');
-        modalBody.className = 'modal-body';
-
-        const title = document.createElement('h2');
-        title.className = 'text-uppercase';
-        title.innerText = jsonData.作品名稱;
-
-        const subtitle = document.createElement('p');
-        subtitle.className = 'item-intro text-muted';
-        subtitle.innerText = jsonData.副標題;
-
+    jsonDataArray.slice(0,n).forEach((jsonData, index) => {
+      const image_item = [];
+      for(let i = 0; i <=6 ; i++){
         const image = document.createElement('img');
         image.className = 'img-fluid d-block mx-auto';
-        image.setAttribute('src', jsonData.圖片連結);
-        image.setAttribute('alt', 'Shirts on a hanger');
+        image.setAttribute('src', jsonData.圖片連結 + `${i}.jpg`);
+        image.onerror = function(){
+          image.remove();
+        }
+        image_item.push(image);
+        console.log(image);
+      }
 
-        const description = document.createElement('p');
-        description.innerText = jsonData.描述;
+      const modal = document.createElement('div');
+      modal.className = 'portfolio-modal modal fade';
+      modal.id = (`p${index + 1}`);
+      modal.tabIndex = '-1';
+      modal.role = 'dialog';
+      modal.style.display = 'none';
+      modal.setAttribute('aria-hidden', 'true');
 
-        const ul = document.createElement('ul');
-        ul.className = 'list-inline';
+      const modalDialog = document.createElement('div');
+      modalDialog.className = 'modal-dialog';
 
-        const dateLi = document.createElement('li');
-        dateLi.innerText = `日期: ${jsonData.日期}`;
+      const modalContent = document.createElement('div');
+      modalContent.className = 'modal-content';
 
-        const clientLi = document.createElement('li');
-        clientLi.innerText = `客戶: ${jsonData.客戶}`;
+      const closeButtonContainer = document.createElement('div');
+      closeButtonContainer.style.position = 'relative';
 
-        const categoryLi = document.createElement('li');
-        categoryLi.innerText = `種類: ${jsonData.種類}`;
+      const closeButton = document.createElement('button');
+      closeButton.className = 'btn leave-button';
+      closeButton.setAttribute('data-dismiss', 'modal');
+      closeButton.setAttribute('type', 'button');
+      closeButton.setAttribute('onmouseover', "this.style.backgroundColor='lightgray';");
+      closeButton.setAttribute('onmouseout', "this.style.backgroundColor='rgb(255, 255, 255, 0)';");
 
-        ul.appendChild(dateLi);
-        ul.appendChild(clientLi);
-        ul.appendChild(categoryLi);
+      const closeIcon = document.createElement('i');
+      closeIcon.className = 'fas fa-times';
+      closeButton.appendChild(closeIcon);
+      closeButtonContainer.appendChild(closeButton);
 
-        const leaveButton = document.createElement('button');
-        leaveButton.className = 'btn';
-        leaveButton.setAttribute('data-dismiss', 'modal');
-        leaveButton.setAttribute('type', 'button');
-        leaveButton.setAttribute('style', 'color:white; background-color: rgb(104, 95, 95); border: 1px solid whitesmoke;');
-        leaveButton.setAttribute('onmouseover', "this.style.backgroundColor='lightgray'; this.style.borderColor='white';");
-        leaveButton.setAttribute('onmouseout', "this.style.backgroundColor='gray'; this.style.borderColor='whitesmoke';");
+      const container = document.createElement('div');
+      container.className = 'container';
 
-        const leaveIcon = document.createElement('i');
-        leaveIcon.className = 'fas fa-times';
-        leaveButton.appendChild(leaveIcon);
-        leaveButton.appendChild(document.createTextNode('離開'));
+      const row = document.createElement('div');
+      row.className = 'row';
 
-        modalBody.appendChild(title);
-        modalBody.appendChild(subtitle);
-        modalBody.appendChild(image);
-        modalBody.appendChild(description);
-        modalBody.appendChild(ul);
-        modalBody.appendChild(document.createElement('p'));
-        modalBody.appendChild(leaveButton);
+      const col = document.createElement('div');
+      col.className = 'col-lg-8 mx-auto';
 
-        col.appendChild(modalBody);
-        row.appendChild(col);
-        container.appendChild(row);
-        modalContent.appendChild(closeButtonContainer);
-        modalContent.appendChild(container);
-        modalDialog.appendChild(modalContent);
-        modal.appendChild(modalDialog);
+      const modalBody = document.createElement('div');
+      modalBody.className = 'modal-body';
 
-        // 將新建的元素添加到頁面中的適當位置
-        const portfolioContainer = document.querySelector('.portfolio-detail'); // 假設有一個包含這些區塊的容器元素
-        portfolioContainer.appendChild(modal);
+      // const title = document.createElement('h2');
+      // title.className = 'text-uppercase';
+      // title.innerText = jsonData.專案名;
+
+      // const subtitle = document.createElement('p');
+      // subtitle.className = 'item-intro text-muted';
+      // subtitle.innerText = jsonData.副標題;
+      
+      const description = document.createElement('p');
+      description.innerText = splitTextWithPunctuation(jsonData.描述);
+
+      const ul = document.createElement('ul');
+      ul.className = 'list-inline';
+
+      const locateLi = document.createElement('li');
+      locateLi.innerText = `區域： ${jsonData.區域}`;
+
+      const dateLi = document.createElement('li');
+      dateLi.innerText = `年份： ${jsonData.日期}`;
+
+      const sizeLi = document.createElement('li');
+      sizeLi.innerText = `坪數： ${jsonData.坪數}坪`;
+
+      const projectNameLi = document.createElement('li');
+      projectNameLi.innerText = `專案名： ${jsonData.專案名}`;
+
+      ul.appendChild(locateLi);
+      ul.appendChild(dateLi);
+      ul.appendChild(sizeLi);
+      ul.appendChild(projectNameLi);
+
+      const leaveButton = document.createElement('button');
+      leaveButton.className = 'btn';
+      leaveButton.setAttribute('data-dismiss', 'modal');
+      leaveButton.setAttribute('type', 'button');
+      leaveButton.setAttribute('style', 'color:white; background-color: rgb(104, 95, 95); border: 1px solid whitesmoke;');
+      leaveButton.setAttribute('onmouseover', "this.style.backgroundColor='lightgray'; this.style.borderColor='white';");
+      leaveButton.setAttribute('onmouseout', "this.style.backgroundColor='gray'; this.style.borderColor='whitesmoke';");
+
+      const leaveIcon = document.createElement('i');
+      leaveIcon.className = 'fas fa-times';
+      leaveButton.appendChild(leaveIcon);
+      leaveButton.appendChild(document.createTextNode('離開'));
+
+      // modalBody.appendChild(title);
+      // modalBody.appendChild(subtitle);
+      modalBody.appendChild(image_item[0]);
+      modalBody.appendChild(ul);
+      modalBody.appendChild(description);
+      for(let j = 1; j <= 6; j++){
+        if(image_item[j] != null)
+          modalBody.appendChild(image_item[j]);
+      }
+      
+      modalBody.appendChild(document.createElement('p'));
+      modalBody.appendChild(leaveButton);
+
+      col.appendChild(modalBody);
+      row.appendChild(col);
+      container.appendChild(row);
+      modalContent.appendChild(closeButtonContainer);
+      modalContent.appendChild(container);
+      modalDialog.appendChild(modalContent);
+      modal.appendChild(modalDialog);
+
+      // 將新建的元素添加到頁面中的適當位置
+      const portfolioContainer = document.querySelector('.portfolio-detail'); // 假設有一個包含這些區塊的容器元素
+      portfolioContainer.appendChild(modal);
     });
 }
 
@@ -340,7 +393,7 @@ function changeToChinese(element) {
       if (text.innerHTML === 'Workflow') {
         text.originalFontSize = window.getComputedStyle(text).fontSize; // 儲存原始字體大小
         var originalSize = parseFloat(text.originalFontSize);
-        text.innerHTML = '工作流程';
+        text.innerHTML = '服務流程';
         text.style.fontSize = originalSize * 1.3 + 'px'; // 將字體大小設置為原始大小的1.3倍
       }
       if (text.innerHTML === 'Portfolio') {
@@ -379,7 +432,7 @@ function changeToChinese(element) {
 function changeToEnglish(element) {
   if (element) {
       var text = element.getElementsByTagName('a')[0];
-      if (text.innerHTML === '工作流程') {
+      if (text.innerHTML === '服務流程') {
       text.innerHTML = 'Workflow';
       text.style.fontSize = text.originalFontSize; // 恢復原始字體大小
       }
@@ -459,7 +512,7 @@ function portfolio_filter(catergory){
 
       const image = document.createElement('img');
       image.className = 'img-fluid';
-      image.setAttribute('src', jsonData.圖片連結);
+      image.setAttribute('src', jsonData.圖片連結 + `1.jpg`);
       image.setAttribute('alt', '');
 
       link.appendChild(hover);
@@ -469,7 +522,7 @@ function portfolio_filter(catergory){
       caption.className = 'portfolio-caption';
 
       const title = document.createElement('h4');
-      title.innerText = jsonData.作品名稱;
+      title.innerText = jsonData.專案名;
 
       const subtitle = document.createElement('p');
       subtitle.className = 'text-muted';
--- portfolio_description.js
+++ portfolio_description.js
@@ -1,40 +1,44 @@
 //將作品描述寫在這裡
 const description = [
     {
-        "作品名稱": "商業空間1",
-        "副標題": "少即是多，越簡單越能享受純粹1",
-        "圖片連結": "./assets/img/portfolio/sample1.jpg",
+        "專案名": "暮森森",
+        "副標題": "副標題",
+        "圖片連結": "./assets/img/portfolio/w2/",
         "描述": "Use this area to describe your project. Markdown supported. This entry (project1.md) uses links for the image sources. All other projects in the portfolio use local images. Both work just fine! Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "日期": "2023.11",
-        "客戶": "人瑄1",
+        "區域": "宜蘭",
+        "日期": "2022",
+        "坪數": "15",
         "種類": "1"
-    },
-    {
-        "作品名稱": "商業空間2",
-        "副標題": "少即是多，越簡單越能享受純粹2",
-        "圖片連結": "./assets/img/portfolio/sample2.jpg",
-        "描述": "Use this area to describe your project. Markdown supported. This entry (project1.md) uses links for the image sources. All other projects in the portfolio use local images. Both work just fine! Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "日期": "2023.12",
-        "客戶": "人瑄2",
-        "種類": "2"
     },
     // 可以繼續添加更多的作品資料
     {
-        "作品名稱": "商業空間3",
-        "副標題": "少即是多，越簡單越能享受純粹3",
-        "圖片連結": "./assets/img/portfolio/sample3.jpg",
+        "專案名": "暮一暮",
+        "副標題": "副標題",
+        "圖片連結": "./assets/img/portfolio/w3/",
         "描述": "Use this area to describe your project. Markdown supported. This entry (project1.md) uses links for the image sources. All other projects in the portfolio use local images. Both work just fine! Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "日期": "2023.13",
-        "客戶": "人瑄3",
+        "區域": "台北",
+        "日期": "2021",
+        "坪數": "10",
+        "種類": "2"
+    },
+    {
+        "專案名": "一一暮",
+        "副標題": "副標題",
+        "圖片連結": "./assets/img/portfolio/w1/",
+        "描述": "Use this area to describe your project. Markdown supported. This entry (project1.md) uses links for the image sources. All other projects in the portfolio use local images. Both work just fine! Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        "區域": "花蓮",
+        "日期": "2020",
+        "坪數": "25",
         "種類": "3"
     },
     {
-        "作品名稱": "商業空間4",
-        "副標題": "少即是多，越簡單越能享受純粹4",
-        "圖片連結": "./assets/img/portfolio/sample4.jpg",
-        "描述": "Use this area to describe your project. Markdown supported. This entry (project1.md) uses links for the image sources. All other projects in the portfolio use local images. Both work just fine! Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "日期": "2023.14",
-        "客戶": "人瑄4",
+        "專案名": "暮暮森",
+        "副標題": "安静分享的溫暖詩意",
+        "圖片連結": "./assets/img/portfolio/w4/",
+        "描述": "在每一個清晨安静地分享家的溫暖詩意，生活中極美的細節總是這樣令人陶醉。本案業主在忙碌的生活中想追求一個舒適明朗的家，以此消除疲憊感忘記喧囂，現代極簡風背後，體現的正是這種注重生活品味與健康時尚的生活態度♡",
+        "區域": "林口",
+        "日期": "2023",
+        "坪數": "20",
         "種類": "1"
     },
   ];
